8104f9dd619665bd051a0916627b8e310ed1ca32
Tr.Torch <vitochek337@gmail.com>
2022-02-10 13:19:32 +0200

    Created slider

---
+++ src/scripts/script.js
@@ -0,0 +1,52 @@
+const pictures = [
+  'http://placekitten.com/500/300',
+  'http://placekitten.com/500/500',
+  'http://placekitten.com/500/700',
+  'http://placekitten.com/700/700',
+];
+
+const fillSlider = () => {
+  const root = document.querySelector('.slider-line');
+
+  pictures.forEach(element => {
+    const newImg = document.createElement('img');
+
+    newImg.src = element;
+    newImg.alt = 'slider-image';
+    newImg.className = 'slider-item';
+    
+    root.append(newImg);
+  });
+};
+
+fillSlider();
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+const sliderButtonPrev = document.querySelector('.slider-button_prev');
+const sliderButtonNext = document.querySelector('.slider-button_next');
+
+const addEventsToButtons = (button, offsetChange) => {
+  button.addEventListener('click', () => {
+    let oneSliderItemSize = document.querySelector('.slider-item').clientWidth;
+
+    oneSliderItemSize = 100 * oneSliderItemSize / window.innerWidth;
+
+    offset += (oneSliderItemSize * offsetChange);
+
+    if(offset <= oneSliderItemSize * pictures.length * -1) {
+      offset = 0;
+    }
+
+    if(offset > 0.0005) {
+      offset = (pictures.length - 1) * oneSliderItemSize * -1;
+    }
+
+
+    sliderLine.style.left = `${offset}vw`;
+  });
+};
+
+addEventsToButtons(sliderButtonNext, -1);
+addEventsToButtons(sliderButtonPrev, 1);
+
